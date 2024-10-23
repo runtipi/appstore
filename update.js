a2c53c938f145bd9eef36a3a12b3d06410ff574f
Stavros <steveiliop56@gmail.com>
2024-10-23 23:01:11 +0300

    refactor: use node one liner

--- update.js
+++ update.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-const updateConfig = async (packageFile: string, newVersion: string) => {
+const updateConfig = async (packageFile, newVersion) => {
   try {
     const composePath = path.join(__dirname, packageFile);
     const configPath = path.join(path.dirname(composePath), "config.json");
@@ -22,3 +22,5 @@ const packageFile = process.argv[2];
 const newVersion = process.argv[3];
 
 updateConfig(packageFile, newVersion);
+
+// node -e 'const fs = require("fs"); const path = require("path"); const composePath = path.join(__dirname, "{{packageFile}}"); const configPath = path.join(path.dirname(composePath), "config.json"); const config = JSON.parse(fs.readFileSync(configPath, "utf8")); config.version = "{{newVersion}}"; config.tipi_version = config.tipi_version + 1; config.updated_at = new Date().getTime(); fs.writeFileSync(configPath, JSON.stringify(config, null, 2));'
